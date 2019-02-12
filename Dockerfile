0cb27ba7959b1dc532eac914ff127edd58416b7c
Ricardo Gomez Escalante <ricardogomezescalante@gmail.com>
2019-02-12 20:19:02 +0100

    added support for i18n

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ FROM python:3.6
 ENV PYTHONUNBUFFERED 1
 COPY requirements.txt start_db.sh ./
 RUN pip install -r requirements.txt
+RUN apt update && apt -y install gettext
 WORKDIR /code
--- start_db.sh
+++ start_db.sh
@@ -5,5 +5,9 @@ python manage.py makemigrations democracy
 python manage.py sqlmigrate registration 0001
 python manage.py sqlmigrate democracy 0001
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
 
+cd filmdemocracy
+django-admin compilemessages
+cd ..
+
+python manage.py runserver 0.0.0.0:8000
